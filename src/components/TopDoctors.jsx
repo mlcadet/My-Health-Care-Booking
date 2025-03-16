ee6511e96bf6cd0512ceec319c522c26f429eb78
Lucie Cadet <cadet.ml@gmail.com>
2025-03-16 01:01:08 -0400

    TopDoctorMenu

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { doctors } from '../assets/assets_frontend/assets';
+import { useNavigate } from 'react-router-dom';
 
 const TopDoctors = () => {
+
+const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center gap-4 py-16 text-gray-900 md:mx-10">
       {/* Header */}
@@ -15,6 +19,7 @@ const TopDoctors = () => {
         {doctors.slice(0, 10).map((item, index) => (
           <div
             key={index}
+            onClick={()=> navigate(`/appointment/${item._id}`)}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500 bg-white"
           >
             {/* Image with responsive height and no crop */}
@@ -46,34 +51,3 @@ const TopDoctors = () => {
 };
 
 export default TopDoctors;
-
-
-// import React from 'react'
-// import { doctors } from '../assets/assets_frontend/assets';
-
-// const TopDoctors = () => {
-//   return (
-//     <div className='flex flex-col items-center gap-4 py-16 text-gray-90 md:mx-10'> 
-//       <h1 className='text-3xl font-medium' >Top Doctors to Book</h1>
-//       <p className='sm:w-1/3 text-center text-sm' >Simply browse through our extensive list of trusted doctors.</p>
-//         <div className='w-full grid grid-cols-auto gap pt-5 gap-y-6 px-3 sm:px-0' >
-//         {doctors.slice(0,10).map((item, index) => (
-
-//           <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
-//             <img className='bg-blue-50' src={item.image} alt="" />
-//             <div className='p-4'>
-//                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-//                     <p className='w-2 h-2 bg-green-500 rounded-full' ></p><p>Available</p>
-//                 </div>
-//                 <p className='text-gray-900 text-lg font-medium' >{item.name}</p>
-//                 <p className='text-gray-600 text-sm' >{item.specialty}</p>
-//             </div>
-//           </div>
-//         ))}
-//         </div>
-//         <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10' >more</button>
-//     </div>
-//   )
-// }
-
-// export default TopDoctors;
